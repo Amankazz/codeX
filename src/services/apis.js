33cd9895ec9052cbf7f7d655830408e7c67aa5ca
Divyansh Kumar <40912399+Amankazz@users.noreply.github.com>
2023-09-20 14:24:27 +0530

    Update apis.js
    change back url

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:4000/api/v1";
+//const BASE_URL = "http://localhost:4000/api/v1";
+const BASE_URL = "https://codex-qimj.onrender.com/api/v1";
 
 // AUTH ENDPOINTS
 export const endpoints = {
